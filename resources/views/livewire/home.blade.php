d6e4ceb7eff315b7c3156dc6630d4c28dc971d90
Anton <loginovanton95@gmail.com>
2024-04-28 23:47:25 +0200

    remove newsletter section for now

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -52,25 +52,6 @@
                 @endforeach
             </div>
             <div class="space-y-10 lg:pl-16 xl:pl-24">
-                <form class="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
-                    <h2 class="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-                        <x-phosphor-envelope class="h-6 w-6 flex-none" />
-                        <span class="ml-3">{{ __('Stay up to date') }}</span>
-                    </h2>
-                    <p class="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-                        {{ __('Get notified when I publish something new, and unsubscribe at any time.') }}
-                    </p>
-                    <div class="mt-6 flex">
-                        <input
-                            type="email"
-                            placeholder="Email address"
-                            aria-label="Email address"
-                            required
-                            class="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"
-                        />
-                        <x-button type="submit" class="ml-4 flex-none">{{ __('Join') }}</x-button>
-                    </div>
-                </form>
                 <div class="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
                     <h2 class="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
                         <x-phosphor-briefcase class="h-6 w-6 flex-none" />
