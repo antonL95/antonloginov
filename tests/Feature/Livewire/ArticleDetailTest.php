7f0728e5a449ef5205e9b2de81c612637c2dd3b1
Anton <loginovanton95@gmail.com>
2024-04-29 17:11:51 +0200

    deploying script and production env

--- tests/Feature/Livewire/ArticleDetailTest.php
+++ tests/Feature/Livewire/ArticleDetailTest.php
@@ -7,6 +7,7 @@ use App\Models\Article;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
+    /** @var Article $article */
     $article = Article::factory()->create();
     Livewire::test(ArticleDetail::class, ['slug' => $article->slug])
         ->assertStatus(200);
