71abb8e5c6ba00180b749adeffa6eb14e7df5c3f
Anton <loginovanton95@gmail.com>
2024-06-12 17:37:59 +0200

    fix pulse command

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable implements FilamentUser
     {
         return str_ends_with($this->email, '@antonloginov.com');
     }
+
+    public function canViewPulse(): bool
+    {
+        return str_ends_with($this->email, '@antonloginov.com');
+    }
 }
