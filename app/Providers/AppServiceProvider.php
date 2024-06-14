ba8416c499ac31e145f3fcb9c42246656022988e
Anton <loginovanton95@gmail.com>
2024-06-14 07:54:31 +0200

    fix app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\URL;
+//use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::define('viewPulse', static fn (User $user) => $user->canViewPulse());
-        URL::forceScheme('https');
+        //URL::forceScheme('https');
     }
 }
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,5 +5,4 @@ declare(strict_types=1);
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
-    Ladumor\LaravelPwa\PWAServiceProvider::class,
 ];
