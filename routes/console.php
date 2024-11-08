abce952df0220b455158185410c73fbe9ba6ae6d
Anton <loginovanton95@gmail.com>
2024-11-08 13:15:55 +0100

    post deploy scripts

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,8 @@
 
 declare(strict_types=1);
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Log;
 
 Artisan::command('inspire', function () {
-    Log::info(Inspiring::quote());
-})->purpose('Display an inspiring quote')->everyMinute();
+    throw new \Exception('This is an inspiration message.');
+})->everyMinute();
