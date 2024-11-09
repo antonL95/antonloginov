3fdb102983af1eb995928f0742e11777f2568632
Anton <loginovanton95@gmail.com>
2024-11-09 11:06:22 +0100

    post deploy scripts

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,3 @@
 <?php
 
 declare(strict_types=1);
-
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Schedule;
-use Sentry\Laravel\Integration;
-
-Schedule::call(function () {
-    Log::error('here!');
-})->everyTwoMinutes();
